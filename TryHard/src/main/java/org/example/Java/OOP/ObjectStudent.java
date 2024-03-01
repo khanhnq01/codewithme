97e02834c6ebb7aed77f67682ceabcf5bc820f92
Quoc Khanh <23730095@ms.uit.edu.vn>
2024-03-01 23:03:48 +0700

    Update Contrucstor Getter and Setter

--- TryHard/src/main/java/org/example/Java/OOP/ObjectStudent.java
+++ TryHard/src/main/java/org/example/Java/OOP/ObjectStudent.java
@@ -2,10 +2,10 @@ package org.example.Java.OOP;
 
 public class ObjectStudent {
     public static void main(String[] args) {
-        Student student = new Student();
-        student.age = 20;
-        student.name = "Khanh";
+        Student student1 = new Student(20, "Khanh");
+        Student student2 = new Student(10, "Khang");
 
-        System.out.println("Hello, " + student.name + ". You are " + student.age + " years old");
+        System.out.println("Hello, " + student1.getName() + ". You are " + student1.getAge() + " years old");
+        System.out.println("Hello, " + student2.getName() + ". You are " + student2.getAge() + " years old");
     }
 }
--- TryHard/src/main/java/org/example/Java/OOP/Student.java
+++ TryHard/src/main/java/org/example/Java/OOP/Student.java
@@ -1,10 +1,29 @@
 package org.example.Java.OOP;
 
 public class Student {
-    int age;
-    String name;
+    private int age;
+    private String name;
 
-    public void Hello(){
-        System.out.println("Hello");
+    public Student(int age, String name){
+        if (age < 0 || age > 100){
+            this.age = 0;
+            throw new IllegalArgumentException("Invalid age");
+        } else {
+            this.age = age;
+        }
+        this.name = name;
     }
+    public int getAge(){
+        return age;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
 }
